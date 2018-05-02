1089a4862d3ff671a8e2cb1032bbb0c80272c738
meakommo <meako98@gmail.com>
2018-05-03 10:59:38 +1200

    added packages

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Home from './Home'
+// import Nav from './Nav'
+// import Footer from './Footer'
+
+const App = props => (
+  <div className='app container'>
+    {/* <Nav /> */}
+    <Home />
+    {/* <Footer /> */}
+  </div>
+)
+
+export default App
---
+++ client/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import {connect} from 'react-redux'
+
+class Home extends React.Component {
+
+  render () {
+    return (
+      <div className="home">
+        <p>Hello World</p>        
+      </div>
+    )
+  }
+}
+
+// function mapStateToProps (state) {
+  
+// }
+
+// export default connect(mapStateToProps)(Home)
+export default Home
