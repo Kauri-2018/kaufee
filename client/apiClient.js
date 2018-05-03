2c056f9e86517c797c27ae06d7f011bc2a6d3772
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-03 15:24:40 +1200

    added registerUser to apiClient

--- client/apiClient.js
+++ client/apiClient.js
@@ -2,5 +2,12 @@ import request from 'superagent'
 
 export function registerUser (userDetails) {
   // Request new user
-  // Returns a promise with new user id and token
+  request.post('/register')
+    .send(userDetails)
+    // Then return the token that was received
+    .then(res => res.body.token)
+    .catch(err => {
+      // TODO catch error properly
+      console.error(err.message)
+    })
 }
