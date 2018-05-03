6ae4d8c0a545ee422ccdac8ad26a77d825eae73b
sarah-mclaughlin <sarahmclaughlin0gmail.com>
2018-05-03 12:00:16 +1200

    fix typos in users.js migration

--- server/db/migrations/20180503105145_users.js
+++ server/db/migrations/20180503105145_users.js
@@ -2,7 +2,7 @@
 exports.up = (knex, Promise) => {
     return knex.schema.createTable('users', (table) => {
         table.increments('id').primary()
-        table.integer('cred_id').refrences(creds.id)
+        table.integer('cred_id').references('creds.id')
         table.string('name')
         table.text('order_text')
     })
