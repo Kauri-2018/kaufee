a47e0774244105ba3101b5ac850dbba5c24af179
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-03 15:32:56 +1200

    added login component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+
 import Register from './Register'
+import Login from './Login'
 
 const App = () => (
-  <Register />
+  <div className='app'>
+    <Register />
+    <Login />
+  </div>
 )
 
 export default App
---
+++ client/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {loginUser} from '../apiClient'
+
+class Login extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  attemptUserLogin () {
+    if (this.state.username && this.state.password) {
+      loginUser({...this.state})
+    }
+  }
+
+  render () {
+    return (
+      <div className ='login'>
+        <span><input name='username' placeholder='Username' onChange={this.handleChange}/></span>
+        <span><input type='password' placeholder='Password' name='password' onChange={this.handleChange}/></span>
+        <button onClick={this.attemptUserLogin}>Log in</button>
+      </div>
+    )
+  }
+}
+
+export default Login
