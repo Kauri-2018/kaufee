2facc207b0cf25e0dbe1841a39745d5e7d6e440c
Zoe Barron <Zoebarron@gmail.com>
2018-05-03 17:44:00 +1200

    updated registerUser function

--- client/apiClient.js
+++ client/apiClient.js
@@ -6,15 +6,11 @@ export const getCurrentOrder = () => {
       return res.body
     })
 }
-    
+
 export function registerUser (userDetails) {
   // Request new user
-  request.post('/register')
+  return request.post('/register')
     .send(userDetails)
     // Then return the token that was received
     .then(res => res.body.token)
-    .catch(err => {
-      // TODO catch error properly
-      console.error(err.message)
-    })
 }
