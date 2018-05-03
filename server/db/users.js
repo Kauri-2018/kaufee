563fd6ae1733920959f89cc390ad086567ba2f09
Frank <xuzy213@gmail.com>
2018-05-03 15:56:14 +1200

    added userExists and createUser function in server/db/users

---
+++ server/db/users.js
@@ -0,0 +1,36 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const knex = require('knex')(config)
+
+function userExists(username, db = knex) {
+  return db('creds')
+    .count('id as n')
+    .where('username', username)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
+
+
+function createUser(username, name, password, db = knex) {
+  const hash = pwhash.generate(password)
+  return db('creds')
+    .insert({
+      username,
+      hash
+    })
+    .then((id) => {
+      db('users')
+      .insert({
+        cred_id: id,
+        name,
+        order_text: ''
+      })
+    })
+}
+
+module.exports = {
+  userExists,
+  createUser
+}
+
