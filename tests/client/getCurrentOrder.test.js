35b91102658d4f66ce8e4995d4fb4c7396805f57
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-05-04 18:36:10 +1200

    add nock object and trial test

--- tests/client/getCurrentOrder.test.js
+++ tests/client/getCurrentOrder.test.js
@@ -1,50 +1,16 @@
-import {getCurrentOrder} from '../../client/apiClient'
+// import {getCurrentOrder} from '../../client/apiClient'
 
 const nock = require('nock')
 
-const getCurrentOrderFn = nock('/api/v1/current-order')
-  .get('/api/v1/current-order')
-  .reply(200, {
-    message: 'Test successful'
-  })
+// const currentOrderNock = nock('http://localhost:3000')
+//   .get('/api/v1/current-order')
+//   .reply(200, {
+//     message: 'Test successful'
+//   })
 
-// jest.mock('../../client/apiClient', () => ({
-//   getCurrentOrder: () => Promise.resolve([
-//     {
-//       orderId: 1,
-//       userName: 'Sarah',
-//       orderDetails: 'flat white',
-//       orderItemsId: 1
-//     }
-//   ])
-// }))
-
-test('getCurrentOrder sends request to server', () => {
-  const expected = 200
-  return getCurrentOrderFn()
-    .then(res => {
-      expect(res.status).toBe(expected)
-    })
-})
-
-// test('getCurrentOrder returns order', () => {
-//   const expected = [
-//     {
-//       orderId: 1,
-//       userName: 'Sarah',
-//       orderDetails: 'flat white',
-//       orderItemsId: 1
-//     }
-//   ]
-//   return getCurrentOrder()
-//     .then(res => {
-//       expect(res).toEqual(expected)
-//     })
-// })
-
-// test('getCurrentOrder sends request to api/v1/current-order', () => {
+// test('getCurrentOrder sends request to server', () => {
 //   const expected = 200
-//   return getCurrentOrder()
+//   return currentOrderNock.done()
 //     .then(res => {
 //       expect(res.status).toBe(expected)
 //     })
