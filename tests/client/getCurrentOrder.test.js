3d20a004e33fc2cf34972b1cd65d9dec4cb095e2
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-05-04 18:05:23 +1200

    add nock as dependency

--- tests/client/getCurrentOrder.test.js
+++ tests/client/getCurrentOrder.test.js
@@ -1,35 +1,51 @@
 import {getCurrentOrder} from '../../client/apiClient'
 
-jest.mock('../../client/apiClient', () => ({
-  getCurrentOrder: () => Promise.resolve([
-    {
-      orderId: 1,
-      userName: 'Sarah',
-      orderDetails: 'flat white',
-      orderItemsId: 1
-    }
-  ])
-}))
+const nock = require('nock')
 
-test('getCurrentOrder returns order', () => {
-  const expected = [
-    {
-      orderId: 1,
-      userName: 'Sarah',
-      orderDetails: 'flat white',
-      orderItemsId: 1
-    }
-  ]
-  return getCurrentOrder()
-    .then(res => {
-      expect(res).toEqual(expected)
-    })
-})
+const getCurrentOrderFn = nock('/api/v1/current-order')
+  .get('/api/v1/current-order')
+  .reply(200, {
+    message: 'Test successful'
+  })
 
-test('getCurrentOrder sends request to api/v1/current-order', () => {
+// jest.mock('../../client/apiClient', () => ({
+//   getCurrentOrder: () => Promise.resolve([
+//     {
+//       orderId: 1,
+//       userName: 'Sarah',
+//       orderDetails: 'flat white',
+//       orderItemsId: 1
+//     }
+//   ])
+// }))
+
+test('getCurrentOrder sends request to server', () => {
   const expected = 200
-  return getCurrentOrder()
+  return getCurrentOrderFn()
     .then(res => {
       expect(res.status).toBe(expected)
     })
 })
+
+// test('getCurrentOrder returns order', () => {
+//   const expected = [
+//     {
+//       orderId: 1,
+//       userName: 'Sarah',
+//       orderDetails: 'flat white',
+//       orderItemsId: 1
+//     }
+//   ]
+//   return getCurrentOrder()
+//     .then(res => {
+//       expect(res).toEqual(expected)
+//     })
+// })
+
+// test('getCurrentOrder sends request to api/v1/current-order', () => {
+//   const expected = 200
+//   return getCurrentOrder()
+//     .then(res => {
+//       expect(res.status).toBe(expected)
+//     })
+// })
