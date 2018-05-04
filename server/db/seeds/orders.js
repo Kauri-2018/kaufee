3f9df43f40c2296b6a5dd4e7dae51e56c1521207
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-05-04 15:15:50 +1200

    seed-data

--- server/db/seeds/orders.js
+++ server/db/seeds/orders.js
@@ -1,18 +1,12 @@
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('orders').del()
-    .then(function () {
+    .then(() => {
       // Inserts seed entries
       return knex('orders').insert([
         {
           id: 1,
           date: 1524718675000,
-          is_complete: true,
-          owner_id: 1
-        },
-        {
-          id: 2,
-          date: 1524545874000,
           is_complete: true,
           owner_id: 1
         }
