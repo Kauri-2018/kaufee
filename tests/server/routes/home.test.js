d9326947bff76c9d85f09cf76c3e80d49bfb7811
Mayur <mayur.h.rana@gmail.com>
2018-05-04 14:45:30 +1200

    add tests for post add to order route

--- tests/server/routes/home.test.js
+++ tests/server/routes/home.test.js
@@ -11,6 +11,18 @@ jest.mock('../../../server/db/orders', () => ({
   ])
 }))
 
+jest.mock('../../../server/db/orderItems', () => ({
+  addToOrder: (userId, orderId) => {
+    if (orderId < 1) {
+      return Promise.reject(new Error('Order does not exist.'))
+    }
+    if (userId < 1) {
+      return Promise.reject(new Error('User does not exist.'))
+    }
+    return Promise.resolve()
+  }
+}))
+
 const server = require('../../../server/server')
 
 test('get /api/v1/current-order returns current order', () => {
@@ -20,5 +32,32 @@ test('get /api/v1/current-order returns current order', () => {
     .set('Accept', 'application/json')
     .then(res => {
       expect(res.body.items[0].order).toBe(expected)
+    })
+})
+
+test('post /api/v1/current-order/order-item/:orderId/:userId add to order', () => {
+  const expected = 200
+  return request(server)
+    .post('/api/v1/current-order/order-item/1/1')
+    .then(res => {
+      expect(res.status).toBe(expected)
+    })
+})
+
+test('post /api/v1/current-order/order-item/0/1 should not add to order', () => {
+  const expected = 500
+  return request(server)
+    .post('/api/v1/current-order/order-item/0/1')
+    .then(res => {
+      expect(res.status).toBe(expected)
+    })
+})
+
+test('post /api/v1/current-order/order-item/1/0 should not add to order', () => {
+  const expected = 500
+  return request(server)
+    .post('/api/v1/current-order/order-item/1/0')
+    .then(res => {
+      expect(res.status).toBe(expected)
     })
 })
