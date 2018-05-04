2df960bb774fafd8656ba067daca9cfc22687d51
Frank <xuzy213@gmail.com>
2018-05-04 13:00:01 +1200

    add jwt into issue

--- server/auth/token.js
+++ server/auth/token.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 const db = require('../db/users')
 
 module.exports = {
@@ -7,10 +9,19 @@ module.exports = {
 function issue (req, res) {
   db.getCredsByName(req.body.username)
     .then(user => {
+      const token = createToken(user, process.env.JWT_SECRET)
       res.json({
         message: 'Authentication successful.',
-        token: user.hash
+        token
       })
     })
 }
- 
+
+function createToken (user, secret) {
+  return jwt.sign({
+    id: user.id,
+    username: user.username
+  }, secret, {
+    expiresIn: '1d'
+  })
+}
