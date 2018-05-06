8d86faeb449ef3a824411df00b79e9021809dc3f
Mayur <mayur.h.rana@gmail.com>
2018-05-07 08:37:54 +1200

    start regsiterUser test

--- tests/client/apiClient/registerUser.test.js
+++ tests/client/apiClient/registerUser.test.js
@@ -1,3 +1,19 @@
 import {registerUser} from '../../../client/apiClient'
 
 const nock = require('nock')
+
+nock('http://localhost')
+  .post('/api/v1/auth/register')
+  .reply(200, {
+    message: 'Test successful'
+  })
+
+test('registerUsers sends post request to server', () => {
+  return registerUser({
+    name: 'Janie',
+    email: 'janie@test.com'
+  })
+    .then(res => {
+      expect(res).toBeTruthy()
+    })
+})
