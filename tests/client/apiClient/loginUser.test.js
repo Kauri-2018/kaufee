8ec2b94f961fb81779cc71ac1acf338c17b9c6f2
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-07 10:44:23 +1200

    add loginUser test

---
+++ tests/client/apiClient/loginUser.test.js
@@ -0,0 +1,21 @@
+import { loginUser } from '../../../client/apiClient'
+
+const nock = require('nock')
+
+const userDetails = {
+  id: 1,
+  name: 'Janie',
+  email: 'janie@test.com',
+  token: 'this is the token'
+}
+
+nock('http://localhost')
+  .post('/api/v1/auth/login')
+  .reply(200, userDetails)
+
+test('loginUser sends post request to server', () => {
+  return loginUser(userDetails)
+    .then(res => {
+      expect(res).toContain('this is the token')
+    })
+})
