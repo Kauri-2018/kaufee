fd073b5836c2a4960cd5c1e51fa98a657eb56c74
sarah-mclaughlin <sarahmclaughlin0gmail.com>
2018-05-07 10:33:40 +1200

    pseudocode profile.test.js routes test file

---
+++ tests/server/routes/profile.tests.js
@@ -0,0 +1,18 @@
+const request = require('supertest')
+
+// jest mock api data
+
+jest.mock('../../../server/db/profile', () => ({
+  // Profile route to mock
+  get: (id) => Promise.resolve([
+    {
+      // TODO return promise as object
+    }
+  ])
+}))
+
+const server = require('../../../server/server')
+
+// get route test
+
+// put route test
