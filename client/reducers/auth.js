05c8077127bc35e715b5ea8c7d6880cb707c8560
Frank Xu <xuzy213@gmail.com>
2018-05-07 10:11:41 +1200

    add auth reducer and login cases

--- client/reducers/auth.js
+++ client/reducers/auth.js
@@ -1,4 +1,4 @@
-
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE} from '../actions/login'
 
 const initState = {
   isFetching: false,
@@ -6,8 +6,29 @@ const initState = {
   errorMessage: ''
 }
 
-export default function(state = initState, action) {
-  switch () {
-
+export default function (state = initState, action) {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        isAuthenticated: action.isAuthenticated
+      }
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        isAuthenticated: true,
+        user: action.user
+      }
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: action.message
+      }
+    default:
+      return state
   }
 }
