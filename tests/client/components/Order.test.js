d7c869e1e6d2381dcb789059bf162932e1ef9a84
Cameron Probert <c.probert@hotmail.co.nz>
2018-05-08 09:54:33 +1200

    added tests for Order component

---
+++ tests/client/components/Order.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+
+import Order from '../../../client/components/Order'
+
+// Create a table to sit the order in so it doesn't complain
+// about having a table row not inside a table
+const testTable = order => (
+  <table>
+    <tbody>
+      <Order {...order} />
+    </tbody>
+  </table>
+)
+
+test('<Order /> onClickFn is called on button click', () => {
+  const trueFn = () => { expect(true).toBeTruthy() }
+
+  const order = {
+    id: 2,
+    name: 'Kale',
+    order: 'flat white',
+    isComplete: false,
+    onClickFn: trueFn
+  }
+
+  const wrapper = mount(testTable(order))
+  const button = wrapper.find('button')
+  button.simulate('click')
+})
+
+test('<Order /> no button when no onClickFn passed as prop', () => {
+  const order = {
+    id: 2,
+    name: 'Kale',
+    order: 'flat white',
+    isComplete: false
+  }
+
+  const wrapper = shallow(testTable(order))
+  expect(wrapper.find('button').length).toBe(0)
+})
