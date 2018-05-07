32e22cd069a83e074c0dd2b6c75d77de04f3e2da
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-05-07 15:17:21 +1200

    fix getCurrentOrderTest

--- tests/client/apiClient/getCurrentOrder.test.js
+++ tests/client/apiClient/getCurrentOrder.test.js
@@ -4,11 +4,19 @@ const nock = require('nock')
 
 nock('http://localhost')
   .get('/api/v1/current-order')
-  .reply(200, 'complete')
+  .reply(200, {
+    isCurrentOrderActive: true,
+    id: 1,
+    items: [{
+      id: 1,
+      name: 'Zoe',
+      order: 'Flat white'
+    }]
+  })
 
 test('getCurrentOrder sends get request to server', () => {
   return getCurrentOrder()
     .then(res => {
-      expect(res).toBeTruthy()
+      expect(res.isCurrentOrderActive).toBeTruthy()
     })
 })
