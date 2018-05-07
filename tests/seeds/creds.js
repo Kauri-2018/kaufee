82e051e9973355333d2caa100ad1cc493b96a862
Zaeburn <zaeburn@outlook.com>
2018-05-08 10:25:14 +1200

    create testing creds.js seed generator

---
+++ tests/seeds/creds.js
@@ -0,0 +1,22 @@
+import {generate} from '../../server/auth/hash'
+
+exports.seed = (knex, Promise) => {
+  // Deletes ALL existing entries
+  return knex('creds').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('creds').insert(generateTestCreds(5))
+    })
+}
+
+function generateTestCreds (numberOfPeople) {
+  const people = []
+  for (let i = 0; i < numberOfPeople; i++) {
+    people.push({
+      id: i,
+      username: i,
+      hash: generate(`Test Person ${i}`)
+    })
+  }
+  return people
+}
