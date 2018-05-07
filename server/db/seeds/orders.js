d323e6c2d9bb7799cb0389407b22d35113a5b772
meakommo <meako98@gmail.com>
2018-05-07 14:56:01 +1200

    changed one seed to is complete true

--- server/db/seeds/orders.js
+++ server/db/seeds/orders.js
@@ -13,7 +13,7 @@ exports.seed = (knex, Promise) => {
         {
           id: 2,
           date: 1524718675000,
-          is_complete: false,
+          is_complete: true,
           owner_id: 1
         }
       ])
